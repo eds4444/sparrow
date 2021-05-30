fde043cec898141d8e336d23493e6180da60da9c
eds4444 <efimov0204@gmail.com>
2021-05-30 18:26:23 +0300

    start5

--- footer.php
+++ footer.php
@@ -43,9 +43,9 @@
    <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
    <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
 
-   <script src="js/jquery.flexslider.js"></script>
-   <script src="js/doubletaptogo.js"></script>
-   <script src="js/init.js"></script>
+
+
+   <?php wp_footer();  ?>
 
 </body>
 
