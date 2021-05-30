7435091f2243a87fb69072480bfb4f90f3d5f409
eds4444 <efimov0204@gmail.com>
2021-05-30 15:52:38 +0300

    start4

--- functions.php
+++ functions.php
@@ -4,5 +4,7 @@ add_action( 'wp_enqueue_scripts', 'style_theme' );
 
 function style_theme(){
 
-    wp_enqueue_style( 'babushka', get_template_directory_uri() . '/assets/css/default.css' );
+    wp_enqueue_style( 'default', get_template_directory_uri() . '/assets/css/default.css' );
+    wp_enqueue_style( 'layout', get_template_directory_uri() . '/assets/css/layout.css' );
+    wp_enqueue_style( 'default', get_template_directory_uri() . '/assets/css/media-queries.css' );
 }
