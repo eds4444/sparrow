999eef17ccf854dd6080d89e688ea42ba5b74eee
eds4444 <efimov0204@gmail.com>
2021-06-06 18:43:38 +0300

    06065

--- category.php
+++ category.php
@@ -30,50 +30,47 @@
               while (have_posts()) {
                  the_post(); ?>
 
-                 <article>
-                  <h1><?php the_title(); ?></h1>
-                  <?php the_content(); ?>
-                 </article>
+               <article class="post">
+
+                 <div class="entry-header cf">
+
+                    <h1><a href="<?php the_permalink(); ?>" title=""><?php the_title(); ?></a></h1>
+
+                    <p class="post-meta">
+
+                       <time class="date" datetime="2014-01-14T11:24"><?php the_time('F jS, Y') ?></time>
+                       /
+                       <span class="categories">
+                       <a href="#">Design</a> /
+                       <a href="#">User Inferface</a> /
+                       <a href="#">Web Design</a>
+                       </span>
+
+                     </p>
+
+                   </div>
+
+      <div class="post-thumb">
+      <a href="single.html" title=""><img src="images/post-image/post-image-1300x500-01.jpg" alt="post-image" title="post-image"></a>
+      </div>
+
+<div class="post-content">
+
+   <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+   nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate
+   cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a
+   ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. </p>
+
+</div>
+
+</article> 
+<!-- post end -->
             <?php
               }
            }
            ?>
 
 
-            <article class="post">
-
-               <div class="entry-header cf">
-
-                  <h1><a href="single.html" title="">Proin gravida nibh vel velit auctor aliquet Aenean sollicitudin auctor.</a></h1>
-
-                  <p class="post-meta">
-
-                     <time class="date" datetime="2014-01-14T11:24">Jan 14, 2014</time>
-                     /
-                     <span class="categories">
-                     <a href="#">Design</a> /
-                     <a href="#">User Inferface</a> /
-                     <a href="#">Web Design</a>
-                     </span>
-
-                  </p>
-
-               </div>
-
-               <div class="post-thumb">
-                  <a href="single.html" title=""><img src="images/post-image/post-image-1300x500-01.jpg" alt="post-image" title="post-image"></a>
-               </div>
-
-               <div class="post-content">
-
-                  <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
-                  nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate
-                  cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a
-                  ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. </p>
-
-               </div>
-
-            </article> <!-- post end -->
 
             <article class="post">
 
